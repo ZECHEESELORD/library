53501da29373aca82794bd8fc6344b80c2655f82
🧀 Harold <55301634+ZECHEESELORD@users.noreply.github.com>
2026-03-25 01:03:56 -0400

    initial setup

---
+++ src/main/java/sh/harold/library/Library.java
@@ -0,0 +1,17 @@
+package sh.harold.library;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Library extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
